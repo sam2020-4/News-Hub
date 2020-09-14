6678bee92a7f83b29b7e580dfb6ebb151bbec5e4
matata samuel <mattasamuel3@gmail.com>
2020-09-14 19:50:56 +0300

    defined the source of the news class

--- app/models.py
+++ app/models.py
@@ -8,4 +8,11 @@ class news:
         self.urlToImage = urlToImage
         self.publishedAt  = publishedAt
 
+# source class starts here which defines the source of objects
+class source:
+    def __init__(self, id,name,url):
+        self.id = id
+        self.name = name
+        self.url = url
+
 
