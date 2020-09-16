3829d3838b29f5ea5f52983abfc7e1d35af24daf
matata samuel <mattasamuel3@gmail.com>
2020-09-16 17:27:53 +0300

    added test instance

--- tests/headline.test.py
+++ tests/headline.test.py
@@ -11,4 +11,13 @@ class headlinesTest(unittest.TestCase):
         set up method that will run before every test
         '''
 
-        self.new_headlines = headlines('')
+        self.new_headlines = headlines("BBC News",
+                                        "Recovering from Covid-19:..",
+                                        "Many patients are...",
+                                        "http://www.bbc.co.uk/news/world-asia-india-54163109",
+                                        "https://ichef.bbci.co.uk/news/1024/branded_news/AE68/production/_114384644_gettyimages-1228467467.jpg",
+                                        "2020-09-16T02:07:23.6661058Z")
+
+        def test_instance(self):
+            '''
+            
